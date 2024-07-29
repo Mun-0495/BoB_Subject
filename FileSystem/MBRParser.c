aa1c55eceac4121fe62868ca2f14fec49588e241
grand6135 <2020028377@hanyang.ac.kr>
2024-07-29 21:43:14 +0900

    Open Source

--- FileSystem/MBRParser.c
+++ FileSystem/MBRParser.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#define SECTOR_SIZE 512
+
 typedef struct {
     uint8_t boot_indicator;
     uint8_t start_chs[3];
@@ -10,6 +12,10 @@ typedef struct {
     uint32_t start_lba;
     uint32_t size_in_sectors;
 } PartitionEntry;
+
+uint64_t offset(uint32_t start_lba) {
+    return (uint64_t)start_lba * SECTOR_SIZE;
+}
 
 void read_mbr(const char *file_path) {
     FILE *file = fopen(file_path, "rb");
@@ -21,27 +27,41 @@ void read_mbr(const char *file_path) {
     unsigned char mbr[512];
     unsigned char ebr[512];
     PartitionEntry partitions[4];
-
     size_t bytes_read = fread(mbr, 1, sizeof(mbr), file);
-    if (bytes_read != sizeof(mbr)) {
-        perror("Error reading MBR");
+    if(bytes_read != sizeof(mbr)) {
+        perror("Error Reading MBR");
         fclose(file);
         return;
+    }
+
+    else if(mbr[510] != 0x55 || mbr[511] != 0xAA) {
+        perror("There is not Signature");
+        fclose(file);
+        return ;
     }
 
     fseek(file, 446, SEEK_SET);  // MBR의 파티션 테이블 시작 위치
     PartitionEntry partitions[4];
     fread(partitions, sizeof(PartitionEntry), 4, file);
 
-    // 파티션 테이블
-    printf("Partition Table (next 64 bytes): ");
-    for (int i = 446; i < 510; i++) {
-        printf("%02X ", mbr[i]);
-    }
-    printf("\n");
+    for(int i = 0; i < 4; i++) {
+        if(partitions[i].partition_type == 0x05) {
+            //TODO : extended MBR
+            //go EBR address
+            //file seek
+            fseek(file, offset(partitions[i].start_lba), SEEK_SET);
+            fread(ebr, 1, sizeof(ebr), file);
+            
+        }
 
-    // 시그니처
-    printf("Signature (last 2 bytes): %02X %02X\n", mbr[510], mbr[511]);
+        if(partitions[i].partition_type == 0x07) {
+            //TODO : just go and idenify the type of Partition.
+            //go EBR address, just print type
+
+        }
+    }
+    // 파티션 테이블
+
 
     fclose(file);
 }
